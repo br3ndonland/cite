873a0d457dae5e0314f210f0fd98664571a54302
Brendon Smith <br3ndonland@protonmail.com>
2020-03-10 17:16:41 -0400

    Format batch edit JavaScript with Prettier
    https://prettier.io/
    www.zotero.org/support/dev/client_coding/javascript_api#batch_editing

--- zotero-batch-edit-publication.js
+++ zotero-batch-edit-publication.js
@@ -1,21 +1,24 @@
-var fieldName = "publicationTitle";
-var oldValue = "Foo";
-var newValue = "Foo2";
- 
-var fieldID = Zotero.ItemFields.getID(fieldName);
-var s = new Zotero.Search();
-s.libraryID = Zotero.Libraries.userLibraryID;
-s.addCondition(fieldName, 'is', oldValue);
-var ids = await s.search();
+var fieldName = "publicationTitle"
+var oldValue = "Foo"
+var newValue = "Foo2"
+
+var fieldID = Zotero.ItemFields.getID(fieldName)
+var s = new Zotero.Search()
+s.libraryID = Zotero.Libraries.userLibraryID
+s.addCondition(fieldName, "is", oldValue)
+var ids = await s.search()
 if (!ids.length) {
-    return "No items found";
+  return "No items found"
 }
-await Zotero.DB.executeTransaction(async function () {
-    for (let id of ids) {
-        let item = await Zotero.Items.getAsync(id);
-        let mappedFieldID = Zotero.ItemFields.getFieldIDFromTypeAndBase(item.itemTypeID, fieldName);
-        item.setField(mappedFieldID ? mappedFieldID : fieldID, newValue);
-        await item.save();
-    }
-});
-return ids.length + " item(s) updated";
+await Zotero.DB.executeTransaction(async function() {
+  for (let id of ids) {
+    let item = await Zotero.Items.getAsync(id)
+    let mappedFieldID = Zotero.ItemFields.getFieldIDFromTypeAndBase(
+      item.itemTypeID,
+      fieldName
+    )
+    item.setField(mappedFieldID ? mappedFieldID : fieldID, newValue)
+    await item.save()
+  }
+})
+return ids.length + " item(s) updated"
